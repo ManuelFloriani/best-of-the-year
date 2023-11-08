486b9d969275d5c6c0cc755f51c0ae9960ea02d5
Bufal8 <125837797+Bufal8@users.noreply.github.com>
2023-11-08 16:37:25 +0100

    New methods which returns a List of item Movies or Songs. New model for them

--- src/main/java/org/java/lessons/springboot/bestoftheyear/controller/IndexController.java
+++ src/main/java/org/java/lessons/springboot/bestoftheyear/controller/IndexController.java
@@ -1,11 +1,13 @@
 package org.java.lessons.springboot.bestoftheyear.controller;
 
+import org.java.lessons.springboot.bestoftheyear.model.Movie;
+import org.java.lessons.springboot.bestoftheyear.model.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.LocalDate;
 import java.time.Year;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,44 +17,71 @@ import java.util.List;
 @RequestMapping("/")
 public class IndexController {
     private static final String name = "Manuel";
-    @GetMapping("/")
-    public String index(Model model){
-        //prendo l'anno corrente
-        int year = Year.now().getValue();
-        // prendo il mio nome
 
-        // li assegno a degli attributi
+    @GetMapping("/")
+    public String index(Model model) {
+        int year = Year.now().getValue();
         model.addAttribute("currentYear", year);
         model.addAttribute("myName", name);
+        model.addAttribute("movies", getBestMovies());
+        model.addAttribute("songs", getBestSongs());
         return "home";
     }
 
-    //    metodo privato per restituire una lista di titoli di film chiamato getBestMovies()
-    private List<String> getBestMovies(){
-        String[] moviesArray = {"The Godfather", "The Shawshank Redemption", "Schindler's List", "Raging Bull", "Casablanca", "Citizen Kane", "Gone with the Wind", "The Wizard of Oz", "One Flew Over the Cuckoo's Nest", "Lawrence of Arabia"};
-        List<String> movies = new ArrayList<>(Arrays.asList(moviesArray));
-        return movies;
-    }
-    // metodo privato per restituire una lista di titoli di canzoni chiamato getBestSongs()
-    private List<String> getBestSongs(){
-        String[] songsArray = {"Like a Rolling Stone", "Satisfaction", "Imagine", "What's Going On", "Respect", "Good Vibrations", "Johnny B. Goode", "Hey Jude", "Smells Like Teen Spirit", "What'd I Say"};
-        List<String> songs = new ArrayList<>(Arrays.asList(songsArray));
-        return songs;
-    }
-    // metodo publico che risponde all'url "/movies" e restituisce la pagina "movies"
-    @GetMapping("movies")
-    public String movies(Model model) {
-        List<String> myMovies = getBestMovies();
-        String bestMovies = String.join(", ", myMovies);
-        model.addAttribute("movies", bestMovies);
-        return "home";
+    private List<Movie> getBestMovies() {
+        Movie[] moviesArray = {
+                new Movie("01", "The Godfather"),
+                new Movie("02", "Fight Club"),
+                new Movie("03", "The Matrix"),
+                new Movie("04", "Star Wars"),
+                new Movie("05", "Oppenheimer")
+        };
+        return new ArrayList<>(Arrays.asList(moviesArray));
     }
 
-    @GetMapping("songs")
-    public String songs(Model model) {
-        List<String> mySongs = getBestSongs();
-        String bestSongs = String.join(", ", mySongs);
-        model.addAttribute("songs", bestSongs);
-        return "home";
+    private List<Song> getBestSongs() {
+        Song[] songsArray = {
+                new Song("01", "Highway to Hell"),
+                new Song("02", "I Want to Break Free"),
+                new Song("03", "Radioactive"),
+                new Song("04", "Turn Down for What"),
+                new Song("05", "The Final Countdown")
+        };
+        return new ArrayList<>(Arrays.asList(songsArray));
+    }
+
+    @GetMapping("movies/{id}")
+    public String movieDetail(@PathVariable("id") String id, Model model) {
+        Movie movie = getMovieById(id);
+        model.addAttribute("movie", movie);
+        return "movie-details";
+    }
+
+    private Movie getMovieById(String id) {
+        for(Movie movie : getBestMovies()) {
+            if(movie.getId().equals(id)) {
+                return movie;
+            }
+        }
+        return null;
+    }
+
+    @GetMapping("songs/{id}")
+    public String songDetail(@PathVariable("id") String id, Model model) {
+        Song song = getSongById(id);
+        model.addAttribute("song", song);
+        return "song-details";
+    }
+
+    private Song getSongById(String id) {
+        for(Song song : getBestSongs()) {
+            if(song.getId().equals(id)) {
+                return song;
+            }
+        }
+        return null;
     }
 }
+
+
+
